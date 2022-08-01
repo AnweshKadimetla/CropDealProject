07f0511000f669f7b766be38e5c138bb4d56e4e1
AnweshKadimetla <96062793+AnweshKadimetla@users.noreply.github.com>
2022-08-01 15:18:56 +0530

    Add files via upload

--- DealerService/src/main/java/com/example/demo/dealerController/DealerController.java
+++ DealerService/src/main/java/com/example/demo/dealerController/DealerController.java
@@ -2,9 +2,8 @@ package com.example.demo.dealerController;
 
 import java.util.List;
 
-import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -15,38 +14,60 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.dealerEntity.Dealer;
-import com.example.demo.dealerRepository.DealerRepository;
-
-
-
+import com.example.demo.dealerService.DealerService;
 @RestController
-
 @RequestMapping("/dealer")
 public class DealerController {
+	
 	@Autowired
-	private DealerRepository repository;
+	public DealerService dealerService;
 	
-	@PostMapping("/addDealer")
-	public String saveDealer(@RequestBody Dealer dealer) {
-		repository.save(dealer);
-		return "Successfully dealer is added";
-	}
 	@GetMapping("/findAll")
-	public List<Dealer> getDealer(){
-		return repository.findAll();
-	}
-	@GetMapping("/findAll/{id}")
-	public Optional<Dealer> getDealer(@PathVariable int id){
-		return repository.findById(id);
-	}
-	@PutMapping("/updateDealer")
-	public Dealer updateDealer(@RequestBody Dealer dealer) {
-		return repository.save(dealer);
-	}
-	@DeleteMapping("/delete/{id}")
-	public String deleteDealer(@PathVariable int id) {
-		 repository.deleteById(id);
-		 return "Dealer deleted";
+	public List<Dealer> getDealerInfoService(){
+		return dealerService.getDealerInfo();
 	}
 	
+	@GetMapping("/findAll/{id}")  
+	public Dealer getDealerInfoById(@PathVariable("id") int id)   
+	{  
+		return dealerService.getDealerById(id);
+	} 
+	
+	@PostMapping("/addDealer")  
+	public Dealer addDealerInfo(@RequestBody Dealer dealer)   
+	{  
+		dealerService.addDealerInfo(dealer); 
+		return dealer;  
+	}
+	
+	@PutMapping("/updateDealer/{id}")  
+	public Dealer updateDealerInfo(@PathVariable("id") int id,@RequestBody Dealer dealer)   
+	{  
+		dealerService.updateDealerInfo(id,dealer);
+	return dealer;  
+	}  
+	
+	@DeleteMapping("/deleteDealer/{id}")  
+	public void deleteDealerInfo(@PathVariable("id") int id)   
+	{  
+		dealerService.deleteDealerInfo(id);  
+	}  
+	
+	/*
+	 * @GetMapping("/admin-info") public List<AdminEntity> getAdminInfoService(){
+	 * return this.adminrepo.findAll();
+	 * 
+	 * }
+	 * 
+	 * @GetMapping("/admin-info/{id}") public AdminEntity
+	 * getAdminById(@PathVariable(value="id")Integer id) { return
+	 * adminrepo.findById(id); }
+	 * 
+	 * @PostMapping("/admin-save") public AdminEntity addAdminInfo(@RequestBody
+	 * AdminEntity adminEntity) { return this.adminrepo.save(adminEntity); }
+	 * 
+	 * @RequestMapping("/info") public String getAdminInfo() {
+	 * 
+	 * return "This Is Admin-Info"; }
+	 */
 }
