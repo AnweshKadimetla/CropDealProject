9548a96d1eb7970bd1ba46208ea70bd2005e5be5
AnweshKadimetla <96062793+AnweshKadimetla@users.noreply.github.com>
2022-09-02 11:45:18 +0530

    Delete CropService directory

--- CropService/src/main/java/com/example/demo/cropRepository/CropRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.cropRepository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.example.demo.cropEntity.Crop;
-
-public interface CropRepository extends MongoRepository<Crop, Integer>{
-
-}
