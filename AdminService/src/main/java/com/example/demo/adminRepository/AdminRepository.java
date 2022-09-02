6cac7181bc9baed8b4af3aa582af3dfa9d170351
AnweshKadimetla <96062793+AnweshKadimetla@users.noreply.github.com>
2022-09-02 11:47:06 +0530

    Delete AdminService directory

--- AdminService/src/main/java/com/example/demo/adminRepository/AdminRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.adminRepository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.example.demo.adminEntity.Admin;
-
-public interface AdminRepository extends MongoRepository<Admin, Integer>{
-
-}
