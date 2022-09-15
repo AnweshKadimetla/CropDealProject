b8dd12a4f81a04c1bbff1da1c9074d496c551749
AnweshKadimetla <96062793+AnweshKadimetla@users.noreply.github.com>
2022-09-15 16:33:05 +0530

    Add files via upload

--- Cart-Info-service/src/main/java/com/example/demo/entity/CartEntity.java
+++ Cart-Info-service/src/main/java/com/example/demo/entity/CartEntity.java
@@ -1,9 +1,11 @@
 package com.example.demo.entity;
 
+import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 @Document(collection="Cart")
 public class CartEntity {
+	@Id
 	private int id;
 	private String CropName;
 	private String CropType;
