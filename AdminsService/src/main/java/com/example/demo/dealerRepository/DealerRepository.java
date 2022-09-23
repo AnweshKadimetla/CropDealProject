6285e1256f36a916228c1e713edfc098521d1a0a
AnweshKadimetla <96062793+AnweshKadimetla@users.noreply.github.com>
2022-09-23 10:10:03 +0530

    Delete AdminsService directory

--- AdminsService/src/main/java/com/example/demo/dealerRepository/DealerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.dealerRepository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.example.demo.dealerEntity.Dealer;
-
-public interface DealerRepository extends MongoRepository<Dealer,Integer>{
-	
-}
--- AdminsService/src/main/java/com/example/demo/farmerRepository/FarmerRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.farmerRepository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import com.example.demo.farmerEntity.Farmer;
-
-public interface FarmerRepository extends MongoRepository<Farmer, Integer>{
-
-}
